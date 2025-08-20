0877a1308c8e12d888fe5a010ecf63bc9c07bf4d
paulo <pauloulisses1212@gmail.com>
2025-08-20 12:35:22 -0300

    Aulas Alterando e modificando elementos

---
+++ Altrando_manipulando_estilos_elementos/script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('#btn');
+
+// Muda a cor de fundo e a cor do texto do botão
+button.style.backgroundColor = 'blue';
+button.style.color = 'white';
+
+// Adiciona a classe de erro ao botão
+button.classList.add('btn-error');
+
+
+// Remove a classe de erro do botão
+button.classList.remove('btn-error');
+
+button.classList.add('btn-blue');
